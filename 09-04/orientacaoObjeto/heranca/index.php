b7c941ada3a74ba69d2e4a3610a4892550a5bcd4
Evelyn Cassinotte <e.cass03@gmail.com>
2024-09-06 09:31:53 -0300

    add: exercicios

--- 09-04/orientacaoObjeto/heranca/index.php
+++ 09-04/orientacaoObjeto/heranca/index.php
@@ -4,7 +4,31 @@ require_once "Conta.class.php";
 require_once "Poupanca.class.php";
 require_once "Corrente.class.php";
 
-$contaPoupanca = new Poupanca(10,"123","321","João",1000);
+$contaPoupanca = new Poupanca(10,"Caixa Federal","2345-10","João Oliveira",1000);
 
-echo $contaPoupanca->getAniversario()."<br>";
-echo $contaPoupanca->getSaldo();
+echo "<div>";
+echo "Titular: ".$contaPoupanca->getTitular()."<br>";
+echo "Agência: ".$contaPoupanca->getAgencia()."<br>";
+echo "Conta: ".$contaPoupanca->getNumero()."<br>";
+echo "Aniversário: ".$contaPoupanca->getAniversario()."<br>";
+echo "Saldo: ".$contaPoupanca->getSaldo()."<br>";
+echo "</div>";
+
+$contaCorrente = new Corrente(5000,"Banco do Brasil","1234-05","Maria Silva",1400);
+
+echo "<div>";
+echo "Titular: {$contaCorrente->getTitular()}<br>";
+echo "Agência: {$contaCorrente->getAgencia()}<br>";
+echo "Conta: {$contaCorrente->getNumero()}<br>";
+echo "Saldo: {$contaCorrente->getSaldo()}<br>";
+echo "Limite: {$contaCorrente->getLimite()}<br>";
+echo "</div>";
+?>
+
+<style>
+    div{
+        border-bottom: 1px solid black;
+        width: 30%;
+        padding: 10px 0;
+    }
+</style>
---
+++ exercicios/orientacao-objetos/readme.md
@@ -0,0 +1,18 @@
+# Exercício - Orientação a Objeto
+Entrega até 11 de setembro de 2024 às 08:00
+
+## Instruções
+
+- Criar as classes do diagrama em anexo;
+- Criar os métodos gets e setters;
+- Implementar o relacionamento de herança.
+
+### OBSERVAÇÕES:
+Os  relacionamentos, que não forem herança, não devem ser implementados, pois não foram vistos em sala de aula.
+Os métodos, que não forem os gets e setters, não precisam ser implementados.
+
+<div align="center">
+
+![diagrama]()
+
+</div>
